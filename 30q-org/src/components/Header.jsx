2c4f45d2b6cad11435a978e8c85b357454c8a872
Alesk <alexsandro060299@outlook.com>
2022-02-20 01:45:32 -0300

    cria primeira seção e header

---
+++ 30q-org/src/components/Header.jsx
@@ -0,0 +1,77 @@
+import {
+  AppBar, IconButton, Toolbar, Typography, Box, Grow,
+} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const classes = {
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  appbar: {
+    background: 'none',
+  },
+  icon: {
+    color: '#fff',
+    fontSize: '1.5rem',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  colorText: {
+    color: '#41419e',
+  },
+  title: {
+    color: '#fff',
+  },
+  container: {
+    textalign: 'center',
+  },
+  goDown: {
+    color: '#41419e',
+    fontSize: '4rem',
+  },
+};
+function Header() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+  useEffect(() => {
+    setPageLoaded(true);
+  });
+  return (
+    <Box sx={classes.root}>
+      <AppBar sx={classes.appbar} elevation={0}>
+        <Toolbar sx={classes.appbarWrapper}>
+          <Typography variant="h5" sx={classes.appbarTitle}>
+            30Q
+            <span style={classes.colorText}> E-SPORTS</span>
+          </Typography>
+          <IconButton>
+            <MenuIcon sx={classes.icon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Grow in={pageLoaded} {...(pageLoaded ? { timeout: 1000 } : {})}>
+        <Box sx={classes.container}>
+          <Typography variant="h4" sx={classes.title}>
+            Bem vindos a 30Q
+            <br />
+            <span style={classes.colorText}> E-SPORTS</span>
+          </Typography>
+          <IconButton>
+            <ExpandMoreIcon sx={classes.goDown} />
+          </IconButton>
+        </Box>
+      </Grow>
+    </Box>
+  );
+}
+
+export default Header;
--- 30q-org/src/pages/Index.jsx
+++ 30q-org/src/pages/Index.jsx
@@ -1,8 +1,23 @@
+import { Box, CssBaseline } from '@mui/material';
 import React from 'react';
+import Header from '../components/Header';
+
+const backgroundImage = `${process.env.PUBLIC_URL}/assets/dark_bg.jpg`;
+const classes = {
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+};
 
 function Index() {
   return (
-    <div>Início</div>
+    <Box sx={classes.root}>
+      <CssBaseline />
+      <Header />
+    </Box>
   );
 }
 
